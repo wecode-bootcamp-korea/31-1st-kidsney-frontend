55bb5e8589bf65bc6e249e0f06ecf33dda29c493
bboyooning <bboyooning@gmail.com>
2022-03-29 13:02:21 +0900

    Fix: scss 경로 수정

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import Router from './Router';
 import 'Styles/common.scss';
 import 'Styles/reset.scss';
-import 'Styles/variables.scss';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
