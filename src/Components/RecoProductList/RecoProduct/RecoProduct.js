86868b974d0cd3eb143369d4e4318318197e3e08
hyodduru <hoje15v@gmail.com>
2022-03-30 19:26:21 +0900

    Add: heart button toggle 기능 구현

--- src/Components/RecoProductList/RecoProduct/RecoProduct.js
+++ src/Components/RecoProductList/RecoProduct/RecoProduct.js
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import 'Components/RecoProductList/RecoProduct/RecoProduct.scss';
+import React from 'react';
 
 const RecoProduct = ({ product }) => {
-  const { id, name, price, image_url, chracter, detail } = product;
+  const { id, name, price, image_url, character, detail } = product;
+  const [isClicked, setIsClicked] = useState(false);
+  const paintHeartBtn = () => {
+    setIsClicked(cur => !cur);
+  };
 
   return (
-    <div className="RecoProduct">
+    <div className="RecoProduct" onClick={paintHeartBtn}>
       <img alt="thumbnail" src={image_url} />
       <button>
-        <i class="far fa-heart" />
+        <i className={isClicked ? 'fas fa-heart' : 'far fa-heart'} />
       </button>
       <h3>{name}</h3>
       <div className="price">${price}</div>
