68e212f6916e0a30bec0616ce5983b95b7372116
bboyooning <bboyooning@gmail.com>
2022-03-31 13:36:34 +0900

    Add: main 랜딩페이지 레이아웃 및 scss 1차 완성

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import './Main.scss';
 
 const Main = () => {
   return (
     <main className="Main">
-      <div className="mainEvent">
-        <img src="https://i.ibb.co/K6MSKvW/main-event-toys-png.png" />
-      </div>
-      <div className="subEvent">
-        <img src="https://i.ibb.co/ngW27ZG/sub-event-toys.png" />
-      </div>
-      <div className=""></div>
-      <div></div>
+      <section className="mainEvent">
+        <a>
+          <img
+            src="https://i.ibb.co/K6MSKvW/main-event-toys-png.png"
+            alt="main event"
+          />
+        </a>
+      </section>
+      <section className="subEvent">
+        <a>
+          <img
+            src="https://i.ibb.co/ngW27ZG/sub-event-toys.png"
+            alt="sub event"
+          />
+        </a>
+      </section>
+      <section className=""></section>
     </main>
   );
 };
