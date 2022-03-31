4d15222ed17f12f90c0d1ccdfc3da55642b0485e
bboyooning <bboyooning@gmail.com>
2022-03-31 10:04:07 +0900

    Add: Main 랜딩페이지 1차 레이아웃

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Main = () => {
-  return <h1>키즈니 메인페이지입니다</h1>;
+  return (
+    <main className="Main">
+      <div className="mainEvent">
+        <img src="https://i.ibb.co/K6MSKvW/main-event-toys-png.png" />
+      </div>
+      <div className="subEvent">
+        <img src="https://i.ibb.co/ngW27ZG/sub-event-toys.png" />
+      </div>
+      <div className=""></div>
+      <div></div>
+    </main>
+  );
 };
 
 export default Main;
