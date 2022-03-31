fea014f16196da202fa7b525e267966afa05211b
jay yoon <jaytaildish@gmail.com>
2022-03-31 16:41:22 +0900

    FIX:[31기 윤재웅]03/31 3차PR 피드백-Product에서 wish-list연결되는 하트 클릭기능 별도 컴포넌트로 분할하기위해 삭제

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -4,12 +4,7 @@ import './Product.scss';
 
 const Product = ({ product, direction }) => {
   const { id, name, price, image_url } = product;
-  const [isClicked, setIsClicked] = useState(false);
   const navigate = useNavigate();
-
-  const paintHeartBtn = () => {
-    setIsClicked(!isClicked);
-  };
 
   const goToProductSpec = () => {
     navigate(`/products/${id}`, {
@@ -21,9 +16,6 @@ const Product = ({ product, direction }) => {
     <li className={`product ${direction}`}>
       <div className="productCard" onClick={goToProductSpec}>
         <img alt="thumbnail" src={image_url} />
-        <button onClick={paintHeartBtn}>
-          <i className={isClicked ? 'fas fa-heart' : 'far fa-heart'} />
-        </button>
         <h3>{name}</h3>
         <div className="price">${price}</div>
       </div>
