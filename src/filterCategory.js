7517ab9199d9c627d97739690a89cb7f16bc2e84
jay yoon <jaytaildish@gmail.com>
2022-04-03 01:07:41 +0900

    FIX:filterCategory상수데이터 수정

--- src/filterCategory.js
+++ src/filterCategory.js
@@ -2,19 +2,16 @@ const filterCategory = [
   {
     id: 1,
     categoryName: 'TYPE',
-    categoryKey: 'sub',
     categoryAttries: ['top', 'bottom', 'acc'],
   },
   {
     id: 2,
     categoryName: 'SIZE',
-    categoryKey: 'size',
     categoryAttries: ['S', 'M', 'L'],
   },
   {
     id: 3,
     categoryName: 'CHARACTER',
-    categoryKey: 'character',
     categoryAttries: ['행복하냥', '놀고싶냥', '놀랐냥'],
   },
 ];
