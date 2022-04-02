1fdc865dfac1162858c6a91f7415b9fc2f04411b
hyodduru <hoje15v@gmail.com>
2022-04-02 22:00:47 +0900

    Modify : Aside 내에 plus, minus icon 추가

--- src/Pages/ProductList/Components/Aside/Aside.js
+++ src/Pages/ProductList/Components/Aside/Aside.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './Aside.scss';
 
 const Aside = () => {
+  const [clickedList, setIsClickedList] = useState('0');
+  const filterList = [
+    {
+      name: 'product Type',
+    },
+    {
+      name: 'Size',
+    },
+    {
+      name: 'Character',
+    },
+  ];
+
+  const handleClickedList = e => {
+    setIsClickedList(e.target.className.split(' ')[0]);
+  };
+
+  const checkList = i => i === clickedList;
+
   return (
     <aside className="aside">
       <ul className="accordionFilter">
-        <li className="filterName">Product Type </li>
-        <li className="filterName">Size</li>
-        <li className="filterName">Character</li>
+        {filterList.map((li, i) => {
+          return (
+            <li
+              key={i}
+              className={`${i} filterName`}
+              onClick={handleClickedList}
+            >
+              {li.name}
+              <i
+                className={
+                  checkList(`${i}`) ? `${i} fas fa-minus` : `${i} fas fa-plus`
+                }
+              />
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
