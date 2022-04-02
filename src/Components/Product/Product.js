35d3c0f5e381e66b42bd63fb43720048cf437b15
hyodduru <hoje15v@gmail.com>
2022-04-02 21:41:46 +0900

    Add : 상품 이미지 mouveover시 다른이미지로 변경해주는 기능 구현

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Product.scss';
 
 const Product = ({ product, direction }) => {
-  const { id, name, price, image_url, image_urls } = product;
+  const { id, name, price, image_urls } = product;
   const navigate = useNavigate();
+
+  let [imageIdx, setImageIdx] = useState('0');
 
   const goToProductSpec = () => {
     navigate(`/products/${id}`, {
       state: product,
     });
   };
+  const hoverImg = () => {
+    setImageIdx(parseInt(imageIdx) + 1);
+  };
+
+  const leaveImg = () => {
+    setImageIdx(0);
+  };
 
   return (
     <li className={`product ${direction}`}>
-      <div className="productCard" onClick={goToProductSpec}>
-        <img alt="thumbnail" src={image_urls[0]} />
+      <div
+        className="productCard"
+        onMouseEnter={hoverImg}
+        onMouseLeave={leaveImg}
+        onClick={goToProductSpec}
+      >
+        <img alt="thumbnail" src={image_urls[imageIdx]} />
+
         <h3>{name}</h3>
         <div className="price">${price}</div>
       </div>
