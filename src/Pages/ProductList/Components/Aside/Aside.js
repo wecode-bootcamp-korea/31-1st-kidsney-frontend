741a647060600e00e35a269de4bf99fc122cdb9c
jay yoon <jaytaildish@gmail.com>
2022-04-05 03:11:13 +0900

    ADD:좌측 다중필터링 기능 구현

--- src/Pages/ProductList/Components/Aside/Aside.js
+++ src/Pages/ProductList/Components/Aside/Aside.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import AsideFilter from './AsideFilter/AsideFilter';
+import FILTER_CATEGORY from '../../../../filterCategory';
 import './Aside.scss';
 
-const Aside = () => {
+const Aside = ({ handleFilter, filters }) => {
   return (
     <aside className="aside">
       <ul className="accordionFilter">
-        <li className="filterName">Product Type</li>
-        <li className="filterName">Size</li>
-        <li className="filterName">Character</li>
+        {FILTER_CATEGORY.map(filter => {
+          return (
+            <AsideFilter
+              key={filter.id}
+              displayedName={filter.displayedName}
+              categoryName={filter.categoryName}
+              categoryAttries={filter.categoryAttries}
+              filters={filters}
+              handleFilter={handleFilter}
+            />
+          );
+        })}
       </ul>
     </aside>
   );
 };
-
 export default Aside;
---
+++ src/Pages/ProductList/Components/Aside/AsideFilter/AsideFilter.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../Aside.scss';
+
+const AsideFilter = ({
+  displayedName,
+  categoryName,
+  categoryAttries,
+  filters,
+  handleFilter,
+}) => {
+  return (
+    <li className="categoryName">
+      {displayedName}
+      <ul className="categoryAttrContainer">
+        {categoryAttries.map(categoryAttr => {
+          return (
+            <li
+              key={categoryAttr}
+              id={[categoryName, categoryAttr]}
+              className="categoryAttr"
+              onClick={handleFilter}
+            >
+              <input
+                id={categoryAttr + 'checkbox'}
+                type="checkbox"
+                checked={
+                  filters.indexOf(categoryName + ',' + categoryAttr) === -1
+                    ? false
+                    : true
+                }
+                readOnly
+              />
+              <span>{categoryAttr}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </li>
+  );
+};
+export default AsideFilter;
