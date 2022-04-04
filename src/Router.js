51012338d895e3cea05419bf597e70e281315ef9
bboyooning <bboyooning@gmail.com>
2022-04-04 15:03:54 +0900

    Modify: ../../Router.js 수정

--- src/Router.js
+++ src/Router.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Nav from './Components/Nav/Nav';
 import Main from './Pages/Main/Main';
-import LoginModal from './Components/Nav/LoginModal';
-import SignUpModal from './Components/Nav/SignUpModal';
-
+import LoginModal from './Components/Nav/Modal/LoginModal';
+import SignUpModal from './Components/Nav/Modal/SignUpModal';
 import ProductList from './Pages/ProductList/ProductList';
 import ProductSpec from './Pages/ProductList/ProductSpec/ProductSpec';
 import Footer from './Components/Footer/Footer';
