9828c357fa0653c2abf4ca4523429d8698f23ad4
hyodduru <hoje15v@gmail.com>
2022-04-06 17:09:43 +0900

    Fix : sider feedback 반영

--- src/Pages/ProductList/ProductSpec/Review/Review.js
+++ src/Pages/ProductList/ProductSpec/Review/Review.js
@@ -9,21 +9,6 @@ const Review = () => {
   const [isClicked, setIsClicked] = useState(false);
   const [reviewList, setReviewList] = useState([]);
 
-  //product spec으로부터 상세페이지 내에 있는 product에 대한 정보 props로 전달받는다.
-  // 가상의 상품 상세 data
-  const product = {
-    id: 1,
-    name: 'Mick Mouse Bodysuit',
-    price: '100.90',
-    image_url: '/images/Boys/surprised_ballcap.png',
-    image_urls: [
-      'https://i.ibb.co/CmH749R/happiness-tshirts-boy-1.png',
-      'https://i.ibb.co/9HNp5NC/happiness-tshirts-boy-2.png',
-    ],
-    character: 'surprised',
-    detail:
-      'Classic Mickey in his mostfamouse pose passes the Disney tradition to Baby with this two-piece casual set. Includes soft, all-cotton bodysuit and matching shorts.Magic in the detailsTwo-piece set includes bodysuit and shorts Jersey knit bodysuit Classic Mickey screen artShort sleeves Snap back placket Single row of leg snaps Ribbed trim Scoop necklineRed sweat shorts Embroidered classic Mickey appliqué  Elastic waistband Rolled cuffs Pockets Brushed back fleece fabrication interior The bare necessities100% cotton Imported Item No.',
-  };
   const getData = async () => {
     const res = await fetch(`${API.products}/${product.id}`);
     const data = await res.json();
@@ -117,3 +102,19 @@ const Review = () => {
 };
 
 export default Review;
+
+//product spec으로부터 상세페이지 내에 있는 product에 대한 정보 props로 전달받는다.
+// 가상의 상품 상세 data
+const product = {
+  id: 1,
+  name: 'Mick Mouse Bodysuit',
+  price: '100.90',
+  image_url: '/images/Boys/surprised_ballcap.png',
+  image_urls: [
+    'https://i.ibb.co/CmH749R/happiness-tshirts-boy-1.png',
+    'https://i.ibb.co/9HNp5NC/happiness-tshirts-boy-2.png',
+  ],
+  character: 'surprised',
+  detail:
+    'Classic Mickey in his mostfamouse pose passes the Disney tradition to Baby with this two-piece casual set. Includes soft, all-cotton bodysuit and matching shorts.Magic in the detailsTwo-piece set includes bodysuit and shorts Jersey knit bodysuit Classic Mickey screen artShort sleeves Snap back placket Single row of leg snaps Ribbed trim Scoop necklineRed sweat shorts Embroidered classic Mickey appliqué  Elastic waistband Rolled cuffs Pockets Brushed back fleece fabrication interior The bare necessities100% cotton Imported Item No.',
+};
