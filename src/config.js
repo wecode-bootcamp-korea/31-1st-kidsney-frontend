fa5a5f50d1671976686c8770cc603364a6da3a22
hyodduru <hoje15v@gmail.com>
2022-04-06 23:20:52 +0900

    Add : git merge 전 commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-export const BASE_URL = 'http://10.58.2.49:8000';
+export const BASE_URL = 'http://10.58.2.13:8000';
 export const API = {
   products: `${BASE_URL}/products`,
 };
 export const ProductsMainEvent = 'https://i.ibb.co/sQ7D7XJ/001-14.png';
 export const Token =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiaWF0IjoxNjQ5MjM1NTA0LCJleHAiOjE2NDk0MDgzMDR9.Kbj3xetNNRwG_grVx7abDL5EOzJf1BDPd08Itp2RGZw';
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MjUsImlhdCI6MTY0OTI1MzI1NiwiZXhwIjoxNjQ5NDI2MDU2fQ.rt5HFrNkW-MRJPy-_Ov2-EzFpRHRUxfd9iMBowKsndc';
