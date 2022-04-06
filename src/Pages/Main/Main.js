2d317143ec56c2c490d4dce40b7ab2d5041164fb
bboyooning <bboyooning@gmail.com>
2022-04-06 18:31:19 +0900

    Modify: main 레이아웃 내용 추가

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -17,6 +17,26 @@ const Main = () => {
           alt="sub event"
         />
       </div>
+      <div className="thirdEvent">
+        <div className="left img">
+          <img
+            src="https://images.unsplash.com/photo-1607453998774-d533f65dac99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt="third event"
+          />
+        </div>
+        <div className="mid img">
+          <img
+            src="https://images.unsplash.com/photo-1643492483985-74ff085738a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+            alt="third event"
+          />
+        </div>
+        <div className="rigth img">
+          <img
+            src="https://images.unsplash.com/photo-1608790672275-309c02d888ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt="third event"
+          />
+        </div>
+      </div>
       <div>
         <RecoProductList />
       </div>
