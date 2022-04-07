4a257c345bf7e688118417039b2110ecd2d5c267
hyodduru <hoje15v@gmail.com>
2022-04-07 19:23:38 +0900

    Fix : 오류수정

--- src/Pages/WishList/SelectList/SelectList.js
+++ src/Pages/WishList/SelectList/SelectList.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BASE_URL } from '../../../config';
 import Product from '../../../Components/Product/Product';
 import './SelectList.scss';
-
-const SelectList = ({ wishProducts, setWishProducts }) => {
+const SelectList = ({ wishProducts }) => {
+  const [wishListIdx, setWishListIdx] = useState([]);
+  const getWishList = () => {
+    fetch(`${BASE_URL}/users/wishlist`, {
+      headers: {
+        Authorization: localStorage.getItem('token'),
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        setWishListIdx(data.wish_list.map(list => list.product.id));
+      })
+      .catch(error => console.error(error.message));
+  };
+  useEffect(getWishList, []);
   return (
-    <ul className="selectList">
-      {wishProducts.map(product => {
-        return (
-          <Product
-            key={product.id}
-            product={product}
-            isHeart={true}
-            setWishProducts={setWishProducts}
-          >
-            size
-          </Product>
-        );
-      })}
-    </ul>
+    <div className="selectList">
+      <ul className="selectProduct">
+        {wishListIdx.length > 0 &&
+          wishProducts.map(product => (
+            <Product
+              key={product.id}
+              product={product}
+              isHeart={wishListIdx.includes(product.id)}
+            />
+          ))}
+      </ul>
+    </div>
   );
 };
-
 export default SelectList;
